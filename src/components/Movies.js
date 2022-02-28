dc1fdaee529c81075645c40d98ba19cc5c921e20
dhairya-eng <dpparikh09@gmail.com>
2022-02-28 15:49:43 +0530

    api app

---
+++ src/components/Movies.js
@@ -0,0 +1,50 @@
+
+import axios from 'axios';
+import React, { useState ,useEffect} from 'react'
+import "./movie.css"
+
+function Movies() {
+   
+  const[content,setcontent]=useState([])
+   const getApi= async()=>{
+    
+    try{
+      const response= await fetch("Api Url")
+      setcontent(await response.json());
+    //  console.log(response);
+    //  const data= await response.json();
+    //  console.log(data);
+    }
+    catch (error) {
+      console.log("my error is "+ error);
+    }
+   }
+
+    useEffect( ()=>{
+      getApi()
+    },[content])
+
+  return (
+    <div className='row text-center'>
+      {
+        content.map((c)=>{
+          return (
+             <div className='col-md-4'>
+              <div className="card col-lg-4 ml-20" style={{width:18+'rem'}}>
+               <img src={c.avatar_url} class="card-img-top" alt="..."/>
+              <div className="card-body">
+               <a className="card-text">{c.url}</a>
+               <a className='card-text'>{c.organizations_url}</a>
+                </div>
+               </div>
+              </div>  
+          )
+        })
+      }
+
+       
+    </div>
+  )
+}
+
+export default Movies
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./navbar.css"
+function Navbar() {
+  return (
+    <div className='nav'>
+        <h1>Watch Box</h1>
+        <p>start exploring </p>
+    </div>
+  )
+}
+
+export default Navbar
